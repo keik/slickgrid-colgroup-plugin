f23025f7dbb76d346b19bddb8e1dd41268187426
keik <k4t0.kei@gmail.com>
2015-12-14 01:30:29 +0900

    1.0.2

--- dist/slick.colgroup.js
+++ dist/slick.colgroup.js
@@ -497,7 +497,7 @@ function plural(ms, n, name) {
 },{}],4:[function(require,module,exports){
 /**
  * https://github.com/keik/slickgrid-colgroup-plugin
- * @version v1.0.1
+ * @version v1.0.2
  * @author keik <k4t0.kei@gmail.com>
  * @license MIT
  */
